67512948b4254611fcc7892ec8a808adfa749062
Franz Zemen <franzzemen@gmail.com>
2022-12-04 16:23:43 -0500

    type

--- src/tsconfig.ts
+++ src/tsconfig.ts
@@ -1,4 +1,21 @@
 /*
 Created by Franz Zemen 12/04/2022
-License Type: 
+License Type: MIT
 */
+import {CompilerOptions, TypeAcquisition, WatchOptions} from 'typescript';
+
+export interface Reference {
+  path: string;
+  prepend?: boolean;
+}
+
+export interface TsConfig {
+  extends?: string;
+  compilerOptions?: CompilerOptions;
+  include?: string[];
+  exclude?: string[];
+  references?: Reference[];
+  files?: string[];
+  typeAcquisition?: TypeAcquisition
+  watchOptions?: WatchOptions;
+}
